a62a95d32f0fdfffa7901f1ba804acb4b9bac806
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-10 17:32:24 -0400

    seems to work when typing out the function inside useEffect, but still running into the issue of not rendering the API

--- app/src/App.js
+++ app/src/App.js
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 
-import { fetchWeather } from './Action/actions';
+import { fetchWeather, FETCH_WEATHER, FETCH_WEATHER_SUCCESS, FETCH_WEATHER_ERROR } from './Action/actions';
 import Weather from './Components/Weather';
+
+import axios from 'axios'
 
 // function App() {
 function App(fetchWeather, fetchingData, error) {
   useEffect(() => {
-    fetchWeather()
+    //fetchWeather()
+    return dispatch => {
+      dispatch({type: FETCH_WEATHER})
+  
+      axios.get('https://www.metaweather.com/api/location/2357024/')
+        .then(res => {
+          dispatch({ type: FETCH_WEATHER_SUCCESS, payload: res.data})
+        })
+        .catch(err => {
+          dispatch({ type: FETCH_WEATHER_ERROR,
+          payload: {message: 'fetching data was unsuccessful'}})
+        })
+    }
   }, [fetchWeather])
 
   return (
